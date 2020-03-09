e6d202d2895cc17685f638f28fadf28a839e2dfb
Igor Nikitin <nikitin.igor3898@gmail.com>
2020-03-09 21:36:52 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	currentTime := time.Now()
+	loc, _ := time.LoadLocation("UTC")
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatal("Can not receive time with error: ", err)
+	}
+
+	fmt.Println("current time:", currentTime.In(loc))
+	fmt.Println("exact time:", time.Local().In(loc))
 }
